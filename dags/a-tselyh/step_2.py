c8bf115796fa58d9c0604d8a9800f2a18f19e3ac
Anastasia <anansee@gmail.com>
2022-03-23 18:45:03 +0300

    replaced file

---
+++ dags/a-tselyh/step_2.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+def print_date(ds):
+    print(ds)
+    print("This function prints ds")
+
+with DAG( 'Hw 2 anansee',
+          default_args={
+              'depends_on_past': False,
+              'email': ['airflow@example.com'],
+              'email_on_failure': False,
+              'email_on_retry': False,
+              'retries': 1,
+              'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+          },
+    description='DAG for task_1 anansee',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 3, 19),
+    catchup=False,
+    tags=['task1']
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_current_folder',  # id, который будет отображаться в интерфейсе
+        bash_command='pwd',  # какую bash команду выполнить в этом таске
+    )
+
+
+    t2 = PythonOperator(
+        task_id='just_print_date',
+        python_callable=print_date,
+    )
+
+    t1 >> t2
+
+
+
+
+
