761617fdab102a1797fd4e36e99c22be7e6c45bf
Fedor Turchenko <fedorturchenko@yandex.ru>
2022-04-03 00:02:28 +0300

    Created DAG with for-loop

--- dags_examples.py
+++ dags_examples.py
@@ -2,6 +2,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 from datetime import datetime, timedelta
+from textwrap import dedent
 
 with DAG(
     'hw_2_f-turchenko-5',
@@ -38,3 +39,44 @@ with DAG(
     )
 
     t1 >> t2
+
+with DAG(
+    'hw_2_f-turchenko-5',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+},
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    for i in range(30):
+        if i < 10:
+            t1 = BashOperator(
+            task_id="print_task_n_console",
+            bash_command=f"echo {i} ",
+            env={"DATA_INTERVAL_START": date},
+            )
+
+        else:
+            def print_task_n(op_kwargs=i):
+                print(f"task number is {op_kwargs}")
+
+            t2 = PythonOperator(
+                task_id = "print_task_n_python",
+                python_callable = print_task_n
+            )
+
+    t1.doc_md = dedent(
+        """\
+    #### Task Documentation
+    `print_task_n` function **prints** the *number* of task
+
+    """
+    )
