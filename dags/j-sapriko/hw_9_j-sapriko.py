9790788c7b979bd8a6eef5c1259652b6ba475bfc
Bubka Bibo <spinaspinskij@gmail.com>
2022-04-15 17:06:19 +0300

    HW 9

---
+++ dags/j-sapriko/hw_9_j-sapriko.py
@@ -0,0 +1,56 @@
+"""
+XCom
+"""
+
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+
+
+def test_xcom_push():
+    return "Airflow tracks everything"
+
+
+def test_xcom_pull(ti):
+    test_print_pull = ti.xcom_pull(
+        key="return_value",
+        task_ids='test_xcom_push',
+    )
+    print(test_print_pull)
+
+
+#DAG
+with  DAG(
+    'hw_8_j-sapriko',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description = 'HW-step-6',
+    schedule_interval = timedelta(days=1),
+    start_date = datetime(2022, 1, 21),
+    catchup = False,
+    tags = ['hw_8'],
+) as dag:
+
+    dag.doc_md = __doc__
+
+    t1 = PythonOperator(
+        task_id='test_xcom_push',
+        python_callable=test_xcom_push,
+    )
+
+    t2 = PythonOperator(
+        task_id='test_xcom_pull',
+        python_callable=test_xcom_pull,
+    )
+
+    t1 >> t2
+
