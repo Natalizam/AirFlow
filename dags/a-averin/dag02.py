1a69dba6a7039530a698b88183b11dce32ed56b2
viceversag <viceversa@mail.ru>
2022-04-19 15:58:56 +0300

    add hw_2_a-averin

---
+++ dags/a-averin/dag02.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'dag01',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5), 
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 4, 13),
+    catchup=False,
+    tags=['tags'],
+) as dag:
+
+    t1 = BashOperator(task_id='BashOperator', bash_command='pwd')
+
+    def print_ds(ds, **kwargs):
+        print(ds)
+
+    t2 = PythonOperator(task_id='PythonOperator', python_callable=print_ds)
+    
+    t1 >> t2
