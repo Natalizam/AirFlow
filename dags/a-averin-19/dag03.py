1fb320ae9949dbe46e7f308042b211d350a595b8
viceversag <viceversa@mail.ru>
2022-04-19 16:26:31 +0300

    add hw_3_a-averin-19

---
+++ dags/a-averin-19/dag03.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'dag03',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5), 
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 4, 19),
+    catchup=False,
+    tags=['tags'],
+) as dag:
+
+    for i in range(10):
+        t1 = BashOperator(task_id=f'BashOperator_{i+1}', bash_command=f"echo {i}")
+
+        t1.doc_md = dedent(
+        """
+        #### Task Documentation (BashOperator)
+        элементы кода (заключены в кавычки `code`),
+        **полужирный**, *курсив*
+        # обзац (неполный)
+        """
+        )
+        
+    def print_2(task_number):
+        print(f"task number is: {task_number}")
+
+    for i in range(11,31):
+        t2 = PythonOperator(task_id=f'PythonOperator_{i}', python_callable=print_2, op_kwargs={'task_number': i})
+
+        t2.doc_md = dedent(
+        """
+        #### Task Documentation (PythonOperator)
+        элементы кода (заключены в кавычки `code`),
+        **полужирный**, *курсив*
+        # обзац (неполный)
+        """
+        ) 
+    
+    t1 >> t2
