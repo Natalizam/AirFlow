5ba9edbb76d2c377a7eb702ae2c4edaf301fa2e9
Dugar Tsyrenzhapov <tsyrenzhapovdugar@mail.ru>
2022-04-30 17:02:45 +0300

    AirFlow Task 6

--- dags/d.tsyrenzhapov-10/dag_lesson_6.py
+++ dags/d.tsyrenzhapov-10/dag_lesson_6.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 with DAG(
-        'tutorial',
+        'd.tsyrenzhapov-10_task_6',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
