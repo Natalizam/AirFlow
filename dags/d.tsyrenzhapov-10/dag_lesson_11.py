7c327877f38129134790ab19d91f740f67bd8661
Дугар Цыренжапов <tsyrenzhapovdugar@mail.ru>
2022-04-30 16:31:40 +0000

    Upload New File

---
+++ dags/d.tsyrenzhapov-10/dag_lesson_11.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from airflow.model import Variable
+
+def val():
+    is_startml = Variable.get('is_startml')
+    print(is_startml)
+with DAG(
+        'task_11_TD',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='Task 9',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 4, 28),
+        catchup=False,
+        tags=['Dugar']
+
+) as dag:
+    t1 = PythonOperator(
+        task_id='variables',
+        python_callable=val
+    )
