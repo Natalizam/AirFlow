876cc04b8c1fb595d9cd9b1e5615f5a22758c13b
Gladkaya Irina <gladkyir@gmail.com>
2022-05-29 18:04:49 +0300

    hw_6_gladkaja_new

--- dags/i-gladkaja/hw_6_i-gladkaja_1.py
+++ dags/i-gladkaja/hw_6_i-gladkaja_1.py
@@ -1,7 +1,8 @@
 from airflow import DAG
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import timedelta, datetime
-from textwrap import dedent
 
 default_args = {
     'depends_on_past': False,
@@ -11,30 +12,31 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
-with DAG('gladkaja_hw_6',
+
+with DAG('gladkaya_dag_6_1',
          default_args=default_args,
          description='A simple tutorial DAG',
          schedule_interval=timedelta(days=1),
          start_date=datetime(2021, 1, 1),
-         catchup=False) \
-        as dag:
+         catchup=False) as dag:
 
-    def print_task_info(task_number, ts, run_id):
+    def print_task_number(task_number, ts, run_id):
         print(f"task number is: {task_number}")
-        print(f"current date is: {ts}")
+        print(f"ts is: {ts}")
         print(f"run_id is: {run_id}")
+
 
     for i in range(20):
         t1 = PythonOperator(
-            task_id=f't1_iter_task_number {i}',
-            python_callable=print_task_info,
-            op_kwargs={'task_number': i
-                       }
+            task_id=f't1_task_6_iter{i}',
+            python_callable=print_task_number,
+            op_kwargs={'task_number': i}
         )
-    t1.doc_md = dedent(
+       t1.doc_md = dedent(
         '''
-    #### print dynamic argument `task_number`
-    and two positional arguments 'ts' and 'run_id'
-    ''')
+        #### print dynamic argument `task_number`
+        and two positional arguments 'ts' and 'run_id'
+        '''
+       )
 
     t1
