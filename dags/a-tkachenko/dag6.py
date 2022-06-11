238005ad76aaee2f1cc2e0cfb35747d0a2b4f3f1
Anton Tkachenko <not-your-buisness@screw_you.com>
2022-06-12 01:10:50 +0300

    Task 6 (Task 7 - actually)

---
+++ dags/a-tkachenko/dag6.py
@@ -0,0 +1,46 @@
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from airflow import DAG
+
+with DAG(
+    'tutorial',
+    # Параметры по умолчанию для тасок
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    # Описание DAG (не тасок, а самого DAG)
+    description='A simple tutorial DAG',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
+    start_date=datetime(2022, 1, 1),
+    # Запустить за старые даты относительно сегодня
+    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['example'],
+) as dag:
+
+    date = "{{ ds }}"
+    ts = "{{ ts }}"
+    run_id = "{{ run_id }}"
+
+    def print_context(task_number, ts, run_id):
+        print(f'task number is: {task_number}')
+        print(ts, run_id)
+
+    for x in range(20):
+        t_py = PythonOperator(
+            task_id='generic_test_' + str(x + 10),
+            python_callable=print_context,
+            op_kwargs={'task_number': int(x)},
+        )
