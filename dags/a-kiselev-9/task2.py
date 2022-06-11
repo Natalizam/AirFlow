216979d73d1f4498bfa05ce780b89320c8184501
akiselev <kiselev.eco@gmail.com>
2022-06-11 09:17:23 +0200

    task2

--- dags/a-kiselev-9/task2.py
+++ dags/a-kiselev-9/task2.py
@@ -1,37 +1,49 @@
-from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-
-
+from airflow import DAG
 
 with DAG(
-    'task_2',
+    'tutorial',
+    # Параметры по умолчанию для тасок
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
-        'retry_delay': timedelta(minutes=5), # timedelta из пакета datetime
-         },
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    # Описание DAG (не тасок, а самого DAG)
     description='A simple tutorial DAG',
+    # Как часто запускать DAG
     schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
     start_date=datetime(2022, 1, 1),
-    catchup=False
+    # Запустить за старые даты относительно сегодня
+    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['example'],
 ) as dag:
-    t1 = BashOperator(
-        task_id='print_date',
-        bash_command='pwd',
+
+    date = "{{ ds }}"
+
+    t_bash = BashOperator(
+    task_id="test_pwd",
+    bash_command="pwd"
     )
 
-    def printds(ds, **kwargs):
+    def print_context(ds):
+        # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
         print(ds)
-        return ds
+        return 'Whatever you return gets printed in the logs'
 
-    t2 = PythonOperator(
-        task_id='python1',
-        python_callable=printds
+    t_py = PythonOperator(
+        task_id='print_the_context',
+        python_callable=print_context
     )
-
-t1>>t2
+t_bash >> t_py
