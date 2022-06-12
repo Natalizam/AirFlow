21c89f283a54abcc184b29fb4d8d0de675e1decb
Anton Tkachenko <not-your-buisness@screw_you.com>
2022-06-12 12:28:56 +0300

    Task 7 (Task 9 - actually)

---
+++ dags/a-tkachenko/dag7.py
@@ -0,0 +1,46 @@
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from airflow import DAG
+
+
+def xcom_test_push(ti):
+    x_str = "xcom test"
+    ti.xcom_push(
+        key='sample_xcom_key',
+        value=x_str
+    )
+
+def xcom_test_pull(ti):
+    pull_res = ti.xcom_pull(
+        key='sample_xcom_key',
+        task_ids='push_result'
+    )
+    print(pull_res)
+
+with DAG(
+    'xcom_dag',
+    start_date=datetime(2021, 1, 1),
+    max_active_runs=2,
+    schedule_interval=timedelta(minutes=30),
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    catchup=False
+) as dag:
+
+    t_py1 = PythonOperator(
+        task_id='push_result',
+        python_callable=xcom_test_push
+    )
+    t_py2 = PythonOperator(
+        task_id='pull_result',
+        python_callable=xcom_test_pull,
+    )
+
+    t_py1 >> t_py2
