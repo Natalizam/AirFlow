85eaa2b22a7386810e0e7c9360382cf9de07bdd7
Ivan Beresnev <Beresnev27@yandex.ru>
2022-06-13 13:23:08 +0300

    get bash pwd and python date

---
+++ dags/beresnev27/HW 2 beresnev27.py
@@ -0,0 +1,63 @@
+"""
+My frist DAG
+"""
+from datetime import datetime, timedelta
+
+# Для объявления DAG нужно импортировать класс из airflow
+from airflow import DAG
+
+# Операторы - это кирпичики DAG, они являются звеньями в графе
+# Будем иногда называть операторы тасками (tasks)
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+with DAG(
+    'first_DAG_name',
+    # Параметры по умолчанию для тасок
+    default_args={
+        # Если прошлые запуски упали, надо ли ждать их успеха
+        'depends_on_past': False,
+        # Кому писать при провале
+        'email': ['airflow@example.com'],
+        # А писать ли вообще при провале?
+        'email_on_failure': False,
+        # Писать ли при автоматическом перезапуске по провалу
+        'email_on_retry': False,
+        # Сколько раз пытаться запустить, далее помечать как failed
+        'retries': 1,
+        # Сколько ждать между перезапусками
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    # Описание DAG (не тасок, а самого DAG)
+    description='First DAG',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
+    start_date=datetime(2022, 1, 1),
+    # Запустить за старые даты относительно сегодня
+    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['first'],
+) as dag:
+
+    # t1, t2, t3 - это операторы (они формируют таски, а таски формируют даг)
+    t1 = BashOperator(
+        task_id='look_directory',  # id, будет отображаться в интерфейсе
+        bash_command='pwd'  # какую bash команду выполнить в этом таске
+    )
+
+    def print_date(ds):
+        print(ds)
+        # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
+        print('print smtg')
+
+    t2 = PythonOperator(
+        task_id='return_date',
+        python_callable=print_date
+    )
+
+
+t1 >> t2
