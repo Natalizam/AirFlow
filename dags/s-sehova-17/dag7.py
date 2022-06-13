60f6659e73993e0623d19e868bbde5d6d5583975
Sofia Sekhova <s-sekhova@mail.ru>
2022-06-13 20:09:55 +0300

    task8

--- dags/s-sehova-17/dag7.py
+++ dags/s-sehova-17/dag7.py
@@ -1,51 +1,39 @@
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator
-
-from datetime import datetime, timedelta
-from textwrap import dedent
-
+from airflow.operators.python import PythonOperator
 
 with DAG(
-    's-sehova-17',
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-    },
-    description='DAG',
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2022, 1, 1),
-
-    catchup=False,
-    tags=['example'],
+        's-sehova-17-7',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        },
+        description='DAG n-anufrieva_task7',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 3, 22),
+        catchup=False,
+        tags=['s-sehova-17'],
 ) as dag:
-    def print_context(task_number, ts, run_id):
-        print(f"task number is: {task_number}")
+    def task(task_number, ts, run_id):
         print(ts)
         print(run_id)
-        
+        print(f"task number is: {task_number}")
+
+
     for i in range(30):
         if i < 10:
-            t71 = BashOperator(
+            task71 = BashOperator(
                 task_id='bash_task_' + str(i),
                 bash_command=f"echo {i}",
             )
         else:
-            t72 = PythonOperator(
+            task72 = PythonOperator(
                 task_id='python_task_' + str(i),
-                python_callable=print_context,
-                op_kwargs ={'task_number': i},
+                python_callable=task,
+                op_kwargs={'task_number': i},
             )
-        
-t71.doc_md = dedent(
-            f"""\
-            #### Python operator doc
-            **Example task** * python_command * = `task_number({i})`
-            """
-        )
-
-t71>>t72
