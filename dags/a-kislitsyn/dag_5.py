6ffdddf390fa2bc818c1f9497d8b0012447cbbcc
Александр Кислицын <79091333443@yandex.ru>
2022-06-13 10:25:00 +0300

    add template BashOperator

--- dags/a-kislitsyn/dag_5.py
+++ dags/a-kislitsyn/dag_5.py
@@ -17,7 +17,7 @@ with DAG(
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
     description = 'Возьмите BashOperator из второго задания (где создавали task через цикл) и подбросьте туда '
-                  'переменную окружения NUMBER, чье значение будет равно i из цикла. '
+                  'переменную окружения NUMBER,  чье значение будет равно i из цикла. '
                   'Распечатайте это значение в команде, указанной в операторе '
                   '(для этого используйте bash_command="echo $NUMBER"). ',
     schedule_interval=timedelta(days=1),
